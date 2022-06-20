a8fe17d481a579cf5a25361366bf0673f16b9883
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-06-20 17:10:37 -0300

    Foreach e finalizando o curso

--- server.js
+++ server.js
@@ -4,7 +4,27 @@ const app = express();
 app.set("view engine", "ejs"); // falando pro express que a ferramenta que vai renderizar o html e ejs
 
 app.get("/", function (req, res) {
-  res.render("pages/index.ejs");
+  const items = [
+    {
+      title: "D",
+      message: "esenvolver aplicações/serviços de forma fácil"
+    },
+    {
+      title: "E",
+      message: "JS usa JavaScript para renderizar HTML"
+    },
+    {
+      title: "+",
+      message: "ais que legal é aprender NojeJS"
+    }
+  ];
+
+  const subtitle = "O EJS é uma linguagem de modelagem para criação de página HTML utilizando JS";
+
+  res.render("pages/index.ejs", {
+    qualitys: items,
+    subtitle: subtitle
+  });
 })
 
 app.get("/sobre", function (req, res) {
